9c903614c504359eb701d40762032b24f22565f5
Damien Luzzo <damienluzzo@gmail.com>
2021-05-28 17:28:33 -0500

    create react app, import seed colors, add color palette component

---
+++ src/ColorPalette.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class ColorPalette extends Component {
+	render() {
+		return (
+			<div className="ColorPalette">
+				{/* Navbar will go here... */}
+				<div className="ColorPalette-colors">{/* This is where a lot of color boxes will live... */}</div>
+				{/* The Footer will go here... */}
+			</div>
+		);
+	}
+}
+
+export default ColorPalette;
