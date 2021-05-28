e651ecd9fc8b5052663d765cdeb29cd8817366ad
Damien Luzzo <damienluzzo@gmail.com>
2021-05-28 17:47:02 -0500

    add color box component, connect to color palette component, and add some basic styling

--- src/ColorPalette.js
+++ src/ColorPalette.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
+import ColorBox from './ColorBox';
+import './ColorPalette.css';
 
 class ColorPalette extends Component {
 	render() {
+        const colorBoxes = this.props.colors.map(color => (
+            <ColorBox background={color.color} name={color.name} />
+        ));
 		return (
 			<div className="ColorPalette">
 				{/* Navbar will go here... */}
-				<div className="ColorPalette-colors">{/* This is where a lot of color boxes will live... */}</div>
+				<div className="ColorPalette-colors">
+                    {colorBoxes}
+                </div>
 				{/* The Footer will go here... */}
 			</div>
 		);
