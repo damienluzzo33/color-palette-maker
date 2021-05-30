d854fbd27b285f3c4e414b521a691ecfede1bc8e
Damien Luzzo <damienluzzo@gmail.com>
2021-05-30 18:11:48 -0500

    add react-router-dom and set up basic route components

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
 import ColorPalette from './ColorPalette';
 import seedColors from './SEED_COLORS';
 import { generateColorPalette } from './chromaColorHelpers';
@@ -7,7 +8,13 @@ class App extends Component {
 	render() {
 		return (
 			<div>
-				<ColorPalette palette={generateColorPalette(seedColors[0])} />
+				<Switch>
+					<Route exact path="/" render={() => <h1>PALETTE LIST HERE</h1>} />
+					<Route exact path="/palette/:id" render={() => <h1>SOME SPECIFIC PALETTE</h1>} />
+				</Switch>
+				{/* <div>
+					<ColorPalette palette={generateColorPalette(seedColors[0])} />
+				</div> */}
 			</div>
 		);
 	}
