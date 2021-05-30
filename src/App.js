ba20750b0bdc1f5e8c3acc7476c54e89ea65345d
Damien Luzzo <damienluzzo@gmail.com>
2021-05-30 18:34:33 -0500

    fix color palette routes and add switch component

--- src/App.js
+++ src/App.js
@@ -5,12 +5,26 @@ import seedColors from './SEED_COLORS';
 import { generateColorPalette } from './chromaColorHelpers';
 
 class App extends Component {
+	findColorPalette(id) {
+		return seedColors.find(
+			paletteSelection => paletteSelection.id === id
+		);
+	}
+
 	render() {
 		return (
 			<div>
 				<Switch>
 					<Route exact path="/" render={() => <h1>PALETTE LIST HERE</h1>} />
-					<Route exact path="/palette/:id" render={() => <h1>SOME SPECIFIC PALETTE</h1>} />
+					<Route 
+						exact 
+						path="/palette/:id" 
+						render={(routeProps) => (
+							<ColorPalette palette={generateColorPalette(
+								this.findColorPalette(routeProps.match.params.id)
+							)} /> 
+						)}
+					/>
 				</Switch>
 				{/* <div>
 					<ColorPalette palette={generateColorPalette(seedColors[0])} />
