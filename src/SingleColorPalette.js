9c285b6f16f8993516f2ff197688128da99d520f
Damien Luzzo <damienluzzo@gmail.com>
2021-05-31 16:56:27 -0500

    add link to go from SingleColorPalette back to ColorPalette component and add styles to go-back button

--- src/SingleColorPalette.js
+++ src/SingleColorPalette.js
@@ -1,52 +1,55 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import ColorBox from './ColorBox';
 import ColorPaletteFooter from './ColorPaletteFooter';
 import Navbar from './Navbar';
 
 class SingleColorPalette extends Component {
-    constructor(props) {
-        super(props);
-        this._shades = this.collectShades(this.props.palette, this.props.colorId);
-        this.state = {
-            format: "hex"
-        };
-        this.changeFormat = this.changeFormat.bind(this);
-    }
+	constructor(props) {
+		super(props);
+		this._shades = this.collectShades(this.props.palette, this.props.colorId);
+		this.state = {
+			format: 'hex'
+		};
+		this.changeFormat = this.changeFormat.bind(this);
+	}
 
-    collectShades(palette, colorId) {
-        let shadeArray = [];
-        let allColors = palette.colors;
+	collectShades(palette, colorId) {
+		let shadeArray = [];
+		let allColors = palette.colors;
 
-        for (let key in allColors) {
-            shadeArray = shadeArray.concat(
-                allColors[key].filter(color => color.id === colorId)
-            );
-        }
-        return shadeArray.slice(1);
-    }
+		for (let key in allColors) {
+			shadeArray = shadeArray.concat(allColors[key].filter((color) => color.id === colorId));
+		}
+		return shadeArray.slice(1);
+	}
 
-    changeFormat(value) {
+	changeFormat(value) {
 		this.setState({
 			format: value
-		})
+		});
 	}
-    
-    render() {
-        const { format } = this.state;
-        const { paletteName, emoji } = this.props.palette;
-        let shadeBoxes = this._shades.map((c) => (
-            <ColorBox name={c.name} key={c.id} bgColor={c[format]} showMore={false} />
-        ));
-        return (
-            <div className="ColorPalette">
-                <Navbar changeFormat={this.changeFormat} allPaletteColors={false} />
-                <div className="ColorPalette-colors" >
-                    {shadeBoxes}
-                </div>
-                <ColorPaletteFooter paletteName={paletteName} emoji={emoji} />
-            </div>
-        );
-    }
+
+	render() {
+		const { format } = this.state;
+		const { paletteName, emoji, id } = this.props.palette;
+
+		let shadeBoxes = this._shades.map((c) => (
+			<ColorBox name={c.name} key={c.name} bgColor={c[format]} showMore={false} />
+		));
+		return (
+			<div className="ColorPalette SingleColorPalette">
+				<Navbar changeFormat={this.changeFormat} allPaletteColors={false} />
+				<div className="ColorPalette-colors">
+					{shadeBoxes}
+					<div className="SingleColorPalette-go-back ColorBox">
+                        <Link to={`/palette/${id}`} className="SingleColorPalette-back-btn">GO BACK</Link>
+                    </div>
+				</div>
+				<ColorPaletteFooter paletteName={paletteName} emoji={emoji} />
+			</div>
+		);
+	}
 }
 
 export default SingleColorPalette;
