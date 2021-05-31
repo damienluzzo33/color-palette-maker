fb6356f1eced479d12ac24d4fbf77ca150249963
Damien Luzzo <damienluzzo@gmail.com>
2021-05-31 18:25:33 -0500

    make text color dynamic based on luminance and contrast of background colors - with the help of Chroma js

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { CopyToClipboard } from 'react-copy-to-clipboard';
 import { Link } from 'react-router-dom';
+import chroma from 'chroma-js';
 import './ColorBox.css';
 
 class ColorBox extends Component {
@@ -27,23 +28,28 @@ class ColorBox extends Component {
 	render() {
 		const { copied } = this.state;
 		const { name, bgColor, extraUrl, showMore } = this.props;
+
+		const luminance = (chroma(bgColor).luminance());
+		const contrast = chroma.contrast('#fff', bgColor);
+		const giveWhiteText = contrast/luminance > 18;
+
 		return (
 			<CopyToClipboard text={bgColor} onCopy={this.changeCopyState}>
 				<div className="ColorBox" style={{ background: bgColor }}>
 					<div className={`ColorBox-copy-overlay ${copied && 'show'}`} style={{ background: bgColor }} />
-					<div className={`ColorBox-copy-msg ${copied && 'show'}`}>
+					<div className={`ColorBox-copy-msg ${copied && 'show'} ${!giveWhiteText && 'dark-msg'}`}>
 						<h1>COPIED!</h1>
 						<p>{bgColor}</p>
 					</div>
 					<div className="ColorBox-copy">
 						<div className="ColorBox-content">
-							<span>{name}</span>
+							<span className={ (giveWhiteText) && "light-text" } >{name}</span>
 						</div>
-						<button className="ColorBox-copy-btn">Copy</button>
+						<button className={`ColorBox-copy-btn ${!giveWhiteText && 'dark-copy'}`}>Copy</button>
 					</div>
 					{ showMore && 
 						<Link to={extraUrl} onClick={(evt) => evt.stopPropagation()}>
-							<span className="ColorBox-more">More</span>
+							<span className={ (giveWhiteText) ? "ColorBox-more" : "ColorBox-more dark-text" }>More</span>
 						</Link> }
 				</div>
 			</CopyToClipboard>
