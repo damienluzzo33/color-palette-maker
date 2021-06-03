094898fdf7cb117b542a401c1435552b9c524f3a
Damien Luzzo <damienluzzo@gmail.com>
2021-06-03 17:30:53 -0500

    add material ui drawer and buttons, add ChromePicker color picker from react-colors

--- src/NewPaletteForm.js
+++ src/NewPaletteForm.js
@@ -1,13 +1,156 @@
 import React, { Component } from 'react';
+import clsx from 'clsx';
+import { withStyles } from '@material-ui/core/styles';
+import { CssBaseline, AppBar, Toolbar, Divider, IconButton } from '@material-ui/core/';
+import { Drawer, Button, Typography } from '@material-ui/core/';
+import { Menu, ChevronLeft } from '@material-ui/icons/';
+import { ChromePicker } from 'react-color';
+
+const drawerWidth = 400;
+
+const styles = (theme) => ({
+	root: {
+		display: 'flex'
+	},
+	appBar: {
+		transition: theme.transitions.create([ 'margin', 'width' ], {
+			easing: theme.transitions.easing.sharp,
+			duration: theme.transitions.duration.leavingScreen
+		})
+	},
+	appBarShift: {
+		width: `calc(100% - ${drawerWidth}px)`,
+		marginLeft: drawerWidth,
+		transition: theme.transitions.create([ 'margin', 'width' ], {
+			easing: theme.transitions.easing.easeOut,
+			duration: theme.transitions.duration.enteringScreen
+		})
+	},
+	menuButton: {
+		marginRight: theme.spacing(2)
+	},
+	hide: {
+		display: 'none'
+	},
+	drawer: {
+		width: drawerWidth,
+		flexShrink: 0
+	},
+	drawerPaper: {
+		width: drawerWidth
+	},
+	drawerHeader: {
+		display: 'flex',
+		alignItems: 'center',
+		padding: theme.spacing(0, 1),
+		// necessary for content to be below app bar
+		...theme.mixins.toolbar,
+		justifyContent: 'flex-end'
+	},
+	content: {
+		flexGrow: 1,
+		padding: theme.spacing(3),
+		transition: theme.transitions.create('margin', {
+			easing: theme.transitions.easing.sharp,
+			duration: theme.transitions.duration.leavingScreen
+		}),
+		marginLeft: -drawerWidth
+	},
+	contentShift: {
+		transition: theme.transitions.create('margin', {
+			easing: theme.transitions.easing.easeOut,
+			duration: theme.transitions.duration.enteringScreen
+		}),
+		marginLeft: 0
+	}
+});
 
 class NewPaletteForm extends Component {
-    render() {
-        return (
-            <div>
-                <h1>New Palette Form</h1>
-            </div>
-        );
-    }
+	state = {
+		open: false
+	};
+
+	handleDrawerOpen = () => {
+		this.setState({
+			open: true
+		});
+	};
+
+	handleDrawerClose = () => {
+		this.setState({
+			open: false
+		});
+	};
+
+	render() {
+		const { classes } = this.props;
+		const { open } = this.state;
+
+		return (
+			<div className={classes.root}>
+				<CssBaseline />
+				<AppBar
+					position="fixed"
+					className={clsx(classes.appBar, {
+						[classes.appBarShift]: open
+					})}
+				>
+					<Toolbar>
+						<IconButton
+							color="inherit"
+							aria-label="open drawer"
+							onClick={this.handleDrawerOpen}
+							edge="start"
+							className={clsx(classes.menuButton, open && classes.hide)}
+						>
+							<Menu />
+						</IconButton>
+						<Typography variant="h6" noWrap>
+							Persistent drawer
+						</Typography>
+					</Toolbar>
+				</AppBar>
+				<Drawer
+					className={classes.drawer}
+					variant="persistent"
+					anchor="left"
+					open={open}
+					classes={{
+						paper: classes.drawerPaper
+					}}
+				>
+					<div className={classes.drawerHeader}>
+						<IconButton onClick={this.handleDrawerClose}>
+                            <ChevronLeft /> 
+						</IconButton>
+					</div>
+                    <Divider />
+                    <Typography variant="h4">
+                        Design Your Palette
+                    </Typography>
+                    <div>
+                        <Button variant="contained" color="secondary">
+                            Clear Palette
+                        </Button>
+                        <Button variant="contained" color="primary">
+                            Random Color
+                        </Button>
+                    </div>
+                    <ChromePicker color="purple" onChangeComplete={newColor => console.log(newColor)} />
+                    <Button variant="contained" color="primary">
+                        Add Color
+                    </Button>
+				</Drawer>
+				<main
+					className={clsx(classes.content, {
+						[classes.contentShift]: open
+					})}
+				>
+					<div className={classes.drawerHeader} />
+				</main>
+			</div>
+		);
+	}
 }
 
-export default NewPaletteForm;
+export default withStyles(styles, { withTheme: true })(NewPaletteForm);
